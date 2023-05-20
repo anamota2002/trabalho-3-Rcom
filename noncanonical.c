2558fe49fc6e370b4a0b5a254bdc93a577afd78f
anamota2002 <126241623+anamota2002@users.noreply.github.com>
2023-05-20 22:28:25 +0100

    parte2

--- noncanonical.c
+++ noncanonical.c
@@ -8,25 +8,117 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
 
 volatile int STOP=FALSE;
+int Setfim=1;// para o
+typedef enum {
+ start,
+ flag_rcv,
+ a_rcv,
+ c_rcv,
+ bcc_ok,
+ stop
+}statesNames;
+statesNames currentState = start;
+statesNames nextState;
+int mdeSET(unsigned char  *buf, unsigned char *compara)
+{
+	switch(currentState)
+	{
+	case start:
+	if(buf[0]==compara[0])
+	{
+	 nextState=flag_rcv;
+	 printf("0x%.2x\n", buf[0]);
+	}
+	 else 
+	 {
+	  nextState=start;
+	 }
+	 break;
+	
+	
+	 case flag_rcv:
+	 if(buf[0]==compara[1])
+	 {
+	 
+	 printf("0x%.2x\n", buf[0]);
+	   nextState=a_rcv;
+	 }
+	 else if(buf[0]==compara[0])
+	 {
+	 	nextState=flag_rcv;
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 
+	 
+	 
+	 case a_rcv:
+	 if(buf[0]=compara[2])
+	 { 
+	  printf("0x%.2x\n", buf[0]);
+	   nextState=c_rcv;
+	 }
+	 else if(buf[0]=compara[1])
+	 {
+	   nextState=flag_rcv;
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 case c_rcv:
+	 if(buf[0]==compara[3])
+	 {
+	 	 printf("0x%.2x\n", buf[0]);
+	 	nextState=bcc_ok;
+	 }
+	 else if(buf[0]=compara[1])
+	 {
+	   nextState=flag_rcv;
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 
+	 case bcc_ok:
+	 if(buf[0]==compara[4])
+	 {
+	 	printf("0x%.2x\n", buf[0]);
+	 	nextState=stop;
+		Setfim=0;//chegou ao fim
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 case stop:
 
+	 //nextState=start;
+	 printf("fimmmmm\n");
+	 break;
+	}
+	
+	
+	currentState=nextState;
+	
+	
+}
 int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
 
-    if ( (argc < 2) ||
+    /*if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-        exit(1);
+       // exit(1);
     }
 
 
@@ -39,7 +131,7 @@ int main(int argc, char** argv)
     fd = open(argv[1], O_RDWR | O_NOCTTY );
     if (fd < 0) { perror(argv[1]); exit(-1); }
 
-    if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+   if (tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
         perror("tcgetattr");
         exit(-1);
     }
@@ -61,29 +153,57 @@ int main(int argc, char** argv)
     */
 
 
-    tcflush(fd, TCIOFLUSH);
+   tcflush(fd, TCIOFLUSH);
 
-    if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
-        perror("tcsetattr");
+   if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
+       perror("tcsetattr");
         exit(-1);
     }
 
     printf("New termios structure set\n");
-    char buf2[255]
-    while (STOP==FALSE) {       /* loop for input */
-        res = read(fd,buf2,255);   /* returns after 5 chars have been input */
-        buf[res]=0;               /* so we can printf... */
-        printf(":%s:%d\n", buf2, res);
-        if (buf[res]==0) STOP=TRUE;
-    }
+
+   // while (STOP==FALSE) {       /* loop for input */
+     //   res = read(fd,buf,255);   /* returns after 5 chars have been input */
+       // buf[res]=0;               /* so we can printf... */
+        //printf(":%s:%d\n", buf, res);
+        //if (buf[0]=='z') STOP=TRUE;
+   // }
+   unsigned char SET_comparar[5]={0x5c,0x01,0x03,0x01^0x03,0x5c};
+   unsigned char SET_recebido[255];
+   
+   int count=0;
+   int l=0;
+while(Setfim)
+{
+	read(fd,&SET_recebido[count],1);    //le 1
+
+	 mdeSET(SET_recebido,SET_comparar);
+
+}
+mdeSET(SET_recebido,SET_comparar);
+
+if (currentState == stop){
 
 
 
+
+
+
+
+
+
+}
+printf("fugi\n");
+// vamos envaiar agora o UA para confirmar
+  char uasend[255]={0x5c, 0x01,0x07,0x01^0x07,0x5c};// o UA que  vai enviar
+   int n_caracteres=strlen(uasend);
+   int j=0;
+   res=write(fd,uasend,n_caracteres+1);// enviou tudo de uma vez
     /*
     O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião
     */
 
-    tcsetattr(fd,TCSANOW,&oldtio);
+ tcsetattr(fd,TCSANOW,&oldtio);
     close(fd);
     return 0;
 }
--- writenoncanonical.c
+++ writenoncanonical.c
@@ -8,7 +8,6 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
@@ -16,20 +15,126 @@
 #define TRUE 1
 
 volatile int STOP=FALSE;
+//criar os estados da maquina de estado
+typedef enum {
+ start,
+ flag_rcv,
+ a_rcv,
+ c_rcv,
+ bcc_ok,
+ stop
+}stateNames;
+int Uafim=1;// para o
+stateNames currentState = start;
+stateNames nextState;
+int mdeUA(char *buf, char *compara)
+{
+	switch(currentState)
+	{
+	case start:
+	if(buf[0]==compara[0])
+	{
+	 nextState=flag_rcv;
+	 printf("0x%.2x\n", buf[0]);
+	}
+	 else 
+	 {
+	  nextState=start;
+	 }
+	 break;
+	
+	
+	 case flag_rcv:
+	 if(buf[0]==compara[1])
+	 {
+	 
+	 printf("0x%.2x\n", buf[0]);
+	   nextState=a_rcv;
+	 }
+	 else if(buf[0]==compara[0])
+	 {
+	 	nextState=flag_rcv;
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 
+	 
+	 
+	 case a_rcv:
+	 if(buf[0]=compara[2])
+	 { 
+	  printf("0x%.2x\n", buf[0]);
+	   nextState=c_rcv;
+	 }
+	 else if(buf[0]=compara[1])
+	 {
+	   nextState=flag_rcv;
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 case c_rcv:
+	 if(buf[0]==compara[3])
+	 {
+	 	 printf("0x%.2x\n", buf[0]);
+	 	nextState=bcc_ok;
+	 }
+	 else if(buf[0]=compara[1])
+	 {
+	   nextState=flag_rcv;
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 
+	 case bcc_ok:
+	 if(buf[0]==compara[4])
+	 {
+	 	printf("0x%.2x\n", buf[0]);
+	 	nextState=stop;
+		//UAfim=0;//chegou ao fim
+	 }
+	 else
+	 nextState=start;
+	 break;
+	 case stop:
+	 Uafim=0;
+	 //nextState=start;
+	 printf("fimmmmm\n");
+	 break;
+	}
+	
+	
+	currentState=nextState;
+	
+}
+void escreve(int* fd)
+{
+
+ char setsend[255]={0x5c,0x01,0x03,0x01^0x03,0x5c};// criado o set que se vai enviar
+  int n_caracteres=strlen(setsend);
+  int j=0;
+  for(j=0; j<n_caracteres; j++)
+   {
+      write(*fd, &setsend[j], 1); // vai escrever um  caracter de cada vez
+   }
+}
 
 int main(int argc, char** argv)
 {
+//(void) signal(SIGALRM, atende);  // instala rotina que atende interrupcao
     int fd,c, res;
     struct termios oldtio,newtio;
     char buf[255];
     int i, sum = 0, speed = 0;
 
-    if ( (argc < 2) ||
+   /* if ( (argc < 2) ||
          ((strcmp("/dev/ttyS0", argv[1])!=0) &&
           (strcmp("/dev/ttyS1", argv[1])!=0) )) {
         printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-        exit(1);
-    }
+       // exit(1);
+    }*/
 
 
     /*
@@ -68,31 +173,52 @@ int main(int argc, char** argv)
     tcflush(fd, TCIOFLUSH);
 
     if (tcsetattr(fd,TCSANOW,&newtio) == -1) {
-        perror("tcsetattr");
-        exit(-1);
+        //perror("tcsetattr");
+        //exit(-1);
     }
 
     printf("New termios structure set\n");
 
-    char g;
-    while(1){
-      g=getchar();  //ler do terminal
-      if(g==10){
-        strcpy(&g,'\0');
-        break;
-      }
-      res=write(fd,&g,1);
-    }
 
-    for (i = 0; i < 255; i++) {
-        buf[i] = 'a';
-    }
 
+ //   for (i = 0; i < 255; i++) {
+   //     buf[i] = 'a';
+   // }
+   escreve(&fd);  // chama a funcao para escrever no ficheiro uma coisa para depois chamarmos o alarme
+   
+   //vamos ler o UA recerbido
+   char UA_comparar[5]={0x5c, 0x01, 0x07, 0x01^0x07, 0x5c};
+   char UA_recebido[255];
+   read(fd,UA_recebido,255);
+   int count=0;
+   while(Uafim)
+   {
+   	 mdeUA(&UA_recebido[count],UA_comparar);
+   	 count ++;
+   }
+   
+// aqui ja enviou o set ;
+//agora vamos ler o UA mandado pelo o outro e seguir
+//char UA_comparar[5]={0x5c, 0x01, 0x07, 0x06, 0x5c};
+//char UA_recebido[255];
+//int count=0;
+//while(1)
+//{
+	//read(fd,&UA_recebido[count],1);    //le 1
+	
+	 //if(Uafim)
+	 //{
+	 //mdeUA(UA_recebido,UA_comparar);
+	 //}
+	//else
+	  // break;
+
+//}
     /*testing*/
-    buf[25] = '\n';
+    //buf[25] = '\n';
 
-    res = write(fd,buf,255);
-    printf("%d bytes written\n", res);
+    //res = write(fd,buf,255);
+    //printf("%d bytes written\n", res);
 
 
     /*
